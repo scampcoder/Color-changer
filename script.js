aede5549079402ff86de17832ff75eec53ab232b
Victoria Kee <victoriakee95@gmail.com>
2020-06-04 17:39:21 -0500

    first attempt at function for colorchange

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+
 const colors = ['red', 'orange', 'yellow', 'blue', 'green', 'maroon', 'crimson', 'teal', 'purple', 'pink', 'sienna'];
 
 const wrapper = document.getElementById('wrapper');
+
+const button = document.getElementById('changer-colour');
+
+button.addEventListener('click', function(){
+  wrapper.style.backgroundcolor = colors[Math.floor(Math.random() * colors.length)];
+});
